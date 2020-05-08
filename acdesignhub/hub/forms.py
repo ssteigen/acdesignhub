3be8138d777392953dfb3e871e6b7a8794ff4841
Savanah Steigen <savanahsteigen@gmail.com>
2020-05-07 20:48:30 -0400

    Handle form submission

--- acdesignhub/hub/forms.py
+++ acdesignhub/hub/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 
 from .models import Design
@@ -11,6 +13,16 @@ class DesignForm(forms.ModelForm):
             'design_type',
             'design_code',
             'description',
-            'designer',
-            'designer_code',
+            'creator',
+            'creator_code',
         )
+
+    def clean_design_code(self):
+        design_code = self.cleaned_data['design_code']
+        design_code = re.sub('[^0-9A-Z]+', '', design_code.upper())
+        return design_code
+
+    def clean_creator_code(self):
+        creator_code = self.cleaned_data['creator_code']
+        creator_code = re.sub('[^0-9A-Z]+', '', creator_code.upper())
+        return creator_code
--- acdesignhub/hub/views.py
+++ acdesignhub/hub/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+
+
 import django_tables2 as tables
 
 from .models import Design
@@ -14,9 +17,16 @@ class TableView(tables.SingleTableView):
 
 
 def new(request):
-    """A view for uploading a new design."""
-    form = DesignForm()
-
-    return render(request, 'hub/new.html', {
-        'form': form
-    })
+    if request.method == 'POST':
+        form = DesignForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            raise RuntimeError(form.errors)
+    else:
+        """A view for uploading a new design."""
+        form = DesignForm()
+        return render(request, 'hub/new.html', {
+            'form': form
+        })
